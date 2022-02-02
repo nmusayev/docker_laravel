e6f810ab7253690d9f241893192fa278a9097c06
Nadir Musayev <nmusayev.dev@gmail.com>
2022-02-02 10:23:53 +0400

    first commit

---
+++ src/database/seeders/QuoteSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Quote;
+use Illuminate\Database\Seeder;
+
+class QuoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Quote::factory()->times(50)->create();
+    }
+}
